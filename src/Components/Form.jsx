5820778df2b5e9a4e26df842fc9eeea829ff10be
unknown <judgepaulogebe3@gmail.com>
2023-02-06 19:04:12 +0100

    feat: store form data in state

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -11,38 +11,52 @@ Short Hair,
 Light skinned,
 From Akwa Ibom state,
 Muscular...`
-    const skills = `3+ Years TypeScript: I have one
-4+ Years Javascript: I have one
+    const skills = `3+ Years TypeScript: I have one year experience
+4+ Years Javascript: I have two years experience
 From Akwa Ibom state: I am from Cross River...`
-    const [formData, setFormData] = useState({})
-    console.log(formData)
+    const [formData, setFormData] = useState({
+        jobDesc: "",
+        skills: ""
+    })
+    function handleFormChange(event){
+        const {name, value} = event.target
+        setFormData((currFormData) => ({
+            ...currFormData,
+            [name]: value
+        }))
+    }
+
     return (
         <>
             <form className="pt-4 pb-10">
                 <div className="pb-10">
                     <label 
-                        htmlFor="job-desc"
+                        htmlFor="jobDesc"
                         className="text-[1.2rem] md:text-[2rem] font-bold text-[#333333] md:pl-5"
                     >
                         Paste the Job Placement / Description
                     </label>
                     <textarea 
-                        name="job-desc"
+                        name="jobDesc"
                         className="border focus:border-2 border-[#333333] rounded-lg w-full h-[20rem] px-7 py-4 text-xl md:mt-4"
                         placeholder={jobDesc}
+                        value={formData.placement}
+                        onChange={handleFormChange}
                     />
                 </div>
                 <div>
                     <label 
-                        htmlFor="job-desc"
+                        htmlFor="skills"
                         className="text-[1.2rem] md:text-[2rem] font-bold text-[#333333] md:pl-5"
                     >
                         Fill out the skills you don't have
                     </label>
                     <textarea 
-                        name="job-desc"
+                        name="skills"
                         className="border focus:border-2 border-[#333333] rounded-lg w-full h-[10rem] px-7 py-4 text-xl md:mt-4"
                         placeholder={skills}
+                        value={formData.skills}
+                        onChange={handleFormChange}
                     />
                 </div>
                 <div className="text-center pt-10">
