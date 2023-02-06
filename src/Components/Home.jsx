72b6c499c3a8bd1f8286a5413569ffcb9cb6d552
unknown <judgepaulogebe3@gmail.com>
2023-02-06 22:05:53 +0100

    fix: hide unnecessary text on mobile screens

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
             </h1>
             <div className="pt-10 lg:pt-5 px-4 text-justify">
                 <p className="text-2xl md:text-2xl font-semibold">
-                    Welcome to <span className="font-extrabold">JobWriteAI</span>, your ultimate solution for creating professional and personalized cover letters in minutes. <span className="">Say goodbye to the hassle of writing cover letters from scratch and let our cutting-edge AI technology help you land your dream job. With GPT-3 at the forefront of our platform, we generate cover letters that are tailored to your specific experience and qualifications. Simply input the required information, and our AI will do the rest. Start crafting your winning cover letter now!</span>
+                    Welcome to <span className="font-extrabold">JobWriteAI</span>, your ultimate solution for creating professional and personalized cover letters in minutes. <span className="hidden md:inline">Say goodbye to the hassle of writing cover letters from scratch and let our cutting-edge AI technology help you land your dream job. With GPT-3 at the forefront of our platform, we generate cover letters that are tailored to your specific experience and qualifications. Simply input the required information, and our AI will do the rest. Start crafting your winning cover letter now!</span>
                 </p>
             </div>
             <h2 className="text-center pt-16 text-3xl md:text-4xl font-bold">
