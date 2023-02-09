ce9830ad0fa3493de813d7065511222377610557
unknown <judgepaulogebe3@gmail.com>
2023-02-09 23:15:19 +0100

    refactor: create new file for placeholder text
    I created a new file 'placeholder.js' to store information about the placeholder text so the could would look cleaner

---
+++ src/placeholder.js
@@ -0,0 +1,15 @@
+export default {
+    jobDesc: `Entry Level Job Posting Available 
+Looking for Front End Developer 
+3+ years TypeScript, 
+4+ years Javascript,
+28+ years Python and Golang Dev,
+Redux Knowledge helps,
+Short Hair,
+Light skinned,
+From Akwa Ibom state,
+Muscular...`,
+    skills: `3+ Years TypeScript: I have one year experience
+4+ Years Javascript: I have two years experience
+From Akwa Ibom state: I am from Cross River...`
+}
