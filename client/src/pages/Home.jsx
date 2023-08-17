17f4fee451694a16fea2ecc4d6a18ed11b0ed85f
Judge-Paul <judgepaulogebe3@gmail.com>
2023-08-17 21:17:21 +0100

    feat: add hero image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Hero from "../images/hero.webp";
 
 export default function Home() {
   return (
     <div className="bg-[#9fcaff] px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-20">
-      <div className="flex-col md:flex-row">
-        <h2 className="w-full md:w-1/2 font-bold text-4xl md:text-6xl text-[#004fb6]">
-          <span className="md:block">Create an </span>
-          <span className="md:block">Amazing </span>
-          <span className="md:block">Cover Letter </span>
-          <span>Effortlessly</span>
-        </h2>
-        <img src="" alt="" width="" className="w-full md:w-1/2" />
-      </div>
-      <div className="md:w-1/2 mt-7">
-        <p className="text-[#E6F4F1] font-semibold">
-          Paste the text from the job posting into our tool. Our AI analyzes the
-          requirements and crafts a cover letter that sells you as the ideal
-          candidate.
-        </p>
-        <Link
-          to="/form"
-          className="text-[#004fb6] bg-[#fcfcd4] font-bold px-20 py-3 rounded-md mt-5 inline-block w-full md:w-max text-center"
-        >
-          Get Started
-        </Link>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 md:mt-8">
+          <h2 className="font-bold text-4xl md:text-6xl text-[#004fb6]">
+            <span className="md:block">Create an</span>
+            <span className="md:block">Amazing</span>
+            <span className="md:block">Cover Letter</span>
+            <span>Effortlessly</span>
+          </h2>
+          <p className="text-[#3a4688] font-semibold mt-8 text-lg">
+            Paste the text from the job posting into our tool. Our AI analyzes
+            the requirements and crafts a cover letter that sells you as the
+            ideal candidate.
+          </p>
+          <Link
+            to="/form"
+            className="text-[#fcfcd4] bg-[#004fb6] hover:bg-[#003d99] active:bg-[#003c99] font-bold px-20 py-3 rounded-md mt-8 inline-block w-full md:w-max text-center"
+          >
+            Get Started
+          </Link>
+        </div>
+        <img src={Hero} alt="Hero for homepage" className="w-full md:w-1/2" />
       </div>
     </div>
   );
