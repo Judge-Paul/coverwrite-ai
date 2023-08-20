730c17c580a7952ae21948e6badb9f8a40604844
Judge-Paul <judgepaulogebe3@gmail.com>
2023-08-20 22:17:11 +0100

    feat: create mobile navigation

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        archivo: ["Archivo", "sans-serif"],
+        workSans: ["Work Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
