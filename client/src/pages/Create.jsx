e6952070072b2f0fa735ff69bf3fee19622d25b7
Judge-Paul <judgepaulogebe3@gmail.com>
2023-08-21 04:15:25 +0100

    feat: reject non job descriptions

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 import axios from "axios";
 import Modal from "../components/Modal";
+import { Toaster, toast } from "sonner";
 
 export default function Create() {
   const [formData, setFormData] = useState({
@@ -13,6 +15,7 @@ export default function Create() {
   });
   const [generatedText, setGeneratedText] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const validateForm = () => {
     let isValid = true;
@@ -43,22 +46,38 @@ export default function Create() {
   async function handleSubmit(event) {
     event.preventDefault();
     if (validateForm()) {
-      const prompt = `Take this Job Description and Create the perfect Cover Letter for me.\n Some information about me first:\n${formData.additionalInfo}\nCover Letter:\n${formData.description}\n\n`;
+      setIsLoading(true);
+      const prompt = `I am going to pass you some values one is a job description and the next is additional info about myself using those two create the perfect cover letter for the job. If what I pass in the job description section doesn't look like an actual job description don't bother creating the Cover Letter just give me the output "Not a Valid Job Description".\nJob Description: ${formData.description}\nAdditional Info: ${formData.additionalInfo}`;
       try {
         const response = await axios.post("http://localhost:3000/generate", {
           prompt,
         });
-        setGeneratedText(response.data.result[0].candidates[0].output);
-        setShowModal(true);
+
+        const generatedText =
+          response?.data?.result?.[0]?.candidates?.[0]?.output;
+        if (generatedText) {
+          setGeneratedText(generatedText);
+          if (generatedText === "Not a Valid Job Description") {
+            toast.error("Not a Valid Job Description");
+          } else {
+            setShowModal(true);
+            toast.success("Cover Letter Generated");
+          }
+        } else {
+          console.error("Invalid response format");
+        }
       } catch (error) {
-        console.error(error);
+        toast.error(error.message);
+      } finally {
+        setIsLoading(false);
       }
     }
   }
 
   return (
     <main className="bg-[#9fcaff] px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-10">
-      {showModal && (
+      <Toaster position="top-right" richColors />
+      {showModal && generatedText !== "Not a Valid Job Description" && (
         <Modal
           showModal={showModal}
           setShowModal={setShowModal}
@@ -80,7 +99,6 @@ export default function Create() {
           className="w-full p-2 border rounded-md"
           placeholder="Paste the job description here..."
           rows="9"
-          required
         />
         {formErrors.description && (
           <p className="text-red-500 text-sm mt-1">{formErrors.description}</p>
@@ -100,7 +118,6 @@ export default function Create() {
           onChange={handleChange}
           className="w-full p-2 border rounded-md"
           placeholder="I am a recent graduate with a degree in..."
-          required
         />
         {formErrors.additionalInfo && (
           <p className="text-red-500 text-sm mt-1">
@@ -110,9 +127,14 @@ export default function Create() {
 
         <button
           type="submit"
-          className="w-full md:w-max mt-8 px-12 py-3 bg-[#004fb6] text-white text-xl font-semibold rounded-full hover:bg-[#3a4688]"
+          disabled={isLoading}
+          className={`w-full md:w-max mt-8 px-12 py-3 text-white text-xl font-semibold rounded-full ${
+            isLoading
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-[#004fb6] hover:bg-[#3a4688]"
+          } flex items-center justify-center`}
         >
-          Submit
+          {isLoading ? <FaSpinner className="animate-spin mr-2" /> : "Submit"}
         </button>
       </form>
     </main>
