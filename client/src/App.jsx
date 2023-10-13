eb6b35eb9be38035988f6c713cb86c0c4a2fa808
ot-of-fincra <toheeb@fincra.com>
2023-10-13 05:26:21 +0100

    add about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import lazyLoading from "./components/lazyLoading";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import About from "./pages/About";
 
 const Home = lazyLoading(() => import("./pages/Home"));
 const NotFound = lazyLoading(() => import("./pages/NotFound"));
@@ -10,42 +11,50 @@ const Create = lazyLoading(() => import("./pages/Create"));
 
 // Layout component for rendering routes with navbar and footer
 const Layout = ({ children }) => (
-  <div>
-    <Navbar />
-    {children}
-    <Footer />
-  </div>
+    <div>
+        <Navbar />
+        {children}
+        <Footer />
+    </div>
 );
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
-          path="/"
-        />
-        <Route
-          element={
-            <Layout>
-              <Create />
-            </Layout>
-          }
-          path="/create"
-        />
-        <Route
-          element={
-            <Layout>
-              <NotFound />
-            </Layout>
-          }
-          path="/*"
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    element={
+                        <Layout>
+                            <Home />
+                        </Layout>
+                    }
+                    path="/"
+                />
+                <Route
+                    element={
+                        <Layout>
+                            <Create />
+                        </Layout>
+                    }
+                    path="/create"
+                />
+                <Route
+                    element={
+                        <Layout>
+                            <About />
+                        </Layout>
+                    }
+                    path="/about"
+                />
+                <Route
+                    element={
+                        <Layout>
+                            <NotFound />
+                        </Layout>
+                    }
+                    path="/*"
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
