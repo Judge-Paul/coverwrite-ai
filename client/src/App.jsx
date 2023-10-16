f264d05a7a2055ce680e4cef9f3d2ba4d2136db2
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-10-16 14:25:14 +0100

    Revert "add about page, add vscode workspace for prettier disparity  (#22)"
    This reverts commit 897d53fa1e0e705691d6365b87f0e2be95d0b013.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import lazyLoading from "./components/lazyLoading";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import About from "./pages/About";
 
 const Home = lazyLoading(() => import("./pages/Home"));
 const NotFound = lazyLoading(() => import("./pages/NotFound"));
@@ -11,50 +10,42 @@ const Create = lazyLoading(() => import("./pages/Create"));
 
 // Layout component for rendering routes with navbar and footer
 const Layout = ({ children }) => (
-    <div>
-        <Navbar />
-        {children}
-        <Footer />
-    </div>
+  <div>
+    <Navbar />
+    {children}
+    <Footer />
+  </div>
 );
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    element={
-                        <Layout>
-                            <Home />
-                        </Layout>
-                    }
-                    path="/"
-                />
-                <Route
-                    element={
-                        <Layout>
-                            <Create />
-                        </Layout>
-                    }
-                    path="/create"
-                />
-                <Route
-                    element={
-                        <Layout>
-                            <About />
-                        </Layout>
-                    }
-                    path="/about"
-                />
-                <Route
-                    element={
-                        <Layout>
-                            <NotFound />
-                        </Layout>
-                    }
-                    path="/*"
-                />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+          path="/"
+        />
+        <Route
+          element={
+            <Layout>
+              <Create />
+            </Layout>
+          }
+          path="/create"
+        />
+        <Route
+          element={
+            <Layout>
+              <NotFound />
+            </Layout>
+          }
+          path="/*"
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
