377900e32291ba17ea0d308eb881b460a69d9b5d
Joshua Morales <114616062+GrandeMan@users.noreply.github.com>
2024-02-06 03:26:15 +0000

    feat: Add Examples to 'Examples' page -Create Example component and routes to App.jsx
    -Create demo vid and embed in Examples.jsx
    
    -Add generated cover letters to Examples.jsx
    
    -Add breakpoints and adhere to style of other pages
    
    -Build and test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import lazyLoading from "./components/lazyLoading";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Examples from "./pages/Examples";
 import About from "./pages/About";
 
 const Home = lazyLoading(() => import("./pages/Home"));
@@ -11,50 +12,58 @@ const Create = lazyLoading(() => import("./pages/Create"));
 
 // Layout component for rendering routes with navbar and footer
 const Layout = ({ children }) => (
-    <div>
-        <Navbar />
-        {children}
-        <Footer />
-    </div>
+	<div>
+		<Navbar />
+		{children}
+		<Footer />
+	</div>
 );
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    element={
-                        <Layout>
-                            <Home />
-                        </Layout>
-                    }
-                    path="/"
-                />
-                <Route
-                    element={
-                        <Layout>
-                            <Create />
-                        </Layout>
-                    }
-                    path="/create"
-                />
-                <Route
-                    element={
-                        <Layout>
-                            <About />
-                        </Layout>
-                    }
-                    path="/about"
-                />
-                <Route
-                    element={
-                        <Layout>
-                            <NotFound />
-                        </Layout>
-                    }
-                    path="/*"
-                />
-            </Routes>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					element={
+						<Layout>
+							<Home />
+						</Layout>
+					}
+					path="/"
+				/>
+				<Route
+					element={
+						<Layout>
+							<Create />
+						</Layout>
+					}
+					path="/create"
+				/>
+				<Route
+					element={
+						<Layout>
+							<Examples />
+						</Layout>
+					}
+					path="/examples"
+				/>
+				<Route
+					element={
+						<Layout>
+							<About />
+						</Layout>
+					}
+					path="/about"
+				/>
+				<Route
+					element={
+						<Layout>
+							<NotFound />
+						</Layout>
+					}
+					path="/*"
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
