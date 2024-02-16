1202a0e2f803d430db94b26623e9cee5b962a67e
Judge-Paul <judgepaulogebe3@gmail.com>
2024-02-16 20:17:44 +0100

    fix(deploy): use dynamic port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const { rateLimit } = require("express-rate-limit");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const API_KEY = process.env.API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
