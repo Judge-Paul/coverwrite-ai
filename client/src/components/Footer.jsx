32d05b2f1de139888ab48bfffd74c46e193487ca
Judge-Paul <judgepaulogebe3@gmail.com>
2024-05-22 01:57:30 +0100

    chore: update landing page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,76 +3,76 @@ import { FaGithub, FaTwitter, FaYoutube, FaHashnode } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-  const year = new Date().getFullYear();
-  return (
-    <footer className="bg-[#9fcaff] font-workSans backdrop-blur-md pt-12 pb-6">
-      <div className="mx-auto px-4 overflow-hidden sm:px-6 lg:px-8 pt-8 border-t-2 border-white">
-        <nav
-          className="-mx-5 -my-2 flex flex-wrap justify-center"
-          aria-label="Footer"
-        >
-          <Link
-            to="#"
-            className="px-5 py-2 text-base font-semibold text-blue-700 hover:text-blue-500"
-          >
-            About
-          </Link>
-          <Link
-            to="#"
-            className="px-5 py-2 text-base font-semibold text-blue-700 hover:text-blue-500"
-          >
-            Pricing
-          </Link>
-          <Link
-            to="https://github.com/Judge-Paul/coverwrite-ai"
-            className="px-5 py-2 text-base font-semibold text-blue-700 hover:text-blue-500"
-          >
-            Contributing
-          </Link>
-        </nav>
-        <div className="mt-8 flex justify-center space-x-4">
-          <Link
-            to="https://x.com/jadge_dev"
-            className="text-blue-700 hover:bg-blue-500 px-1.5 py-1 rounded-md"
-          >
-            <span className="sr-only">Twitter</span>
-            <FaTwitter className="h-6 w-6" />
-          </Link>
-          <Link
-            to="https://github.com/Judge-Paul"
-            className="text-blue-700 hover:bg-blue-500 px-1.5 py-1 rounded-md"
-          >
-            <span className="sr-only">GitHub</span>
-            <FaGithub className="h-6 w-6" />
-          </Link>
-          <Link
-            to="https://hashnode.com/@jadge"
-            className="text-blue-700 hover:bg-blue-500 px-1.5 py-1 rounded-md"
-          >
-            <span className="sr-only">Hashnode</span>
-            <FaHashnode className="h-6 w-6" />
-          </Link>
-          <Link
-            to="https://youtube.com/@jadge_dev23"
-            className="text-blue-700 hover:bg-blue-500 px-1.5 py-1 rounded-md"
-          >
-            <span className="sr-only">Youtube</span>
-            <FaYoutube className="h-6 w-6" />
-          </Link>
-        </div>
-        <div className="text-center mt-10">
-          <Link
-            to="https://github.com/Judge-Paul/coverwrite-ai/issues"
-            className="mt-5 text-base text-blue-700"
-          >
-            Have a feature you want to add or noticed a bug?{" "}
-            <span className="hover:text-blue-500">Create an Issue</span>
-          </Link>
-        </div>
-        <p className="mt-5 text-center text-base text-blue-700">
-          &copy; {year} CoverWriteAI. All rights reserved.
-        </p>
-      </div>
-    </footer>
-  );
+	const year = new Date().getFullYear();
+	return (
+		<footer className="bg-[#9fcaff] font-workSans backdrop-blur-md pt-12 pb-6">
+			<div className="mx-auto px-4 overflow-hidden sm:px-6 lg:px-8 pt-8 border-t-2 border-white">
+				<nav
+					className="-mx-5 -my-2 flex flex-wrap justify-center"
+					aria-label="Footer"
+				>
+					<Link
+						to="#"
+						className="px-5 py-2 text-base font-semibold text-blue-700 hover:text-blue-500"
+					>
+						About
+					</Link>
+					<Link
+						to="#"
+						className="px-5 py-2 text-base font-semibold text-blue-700 hover:text-blue-500"
+					>
+						Pricing
+					</Link>
+					<Link
+						to="https://github.com/Judge-Paul/coverwrite-ai"
+						className="px-5 py-2 text-base font-semibold text-blue-700 hover:text-blue-500"
+					>
+						Contributing
+					</Link>
+				</nav>
+				<div className="mt-8 flex justify-center space-x-4">
+					<Link
+						to="https://x.com/jadge_dev"
+						className="text-blue-700 hover:text-blue-500 px-1.5 py-1 rounded-md"
+					>
+						<span className="sr-only">Twitter</span>
+						<FaTwitter className="h-6 w-6" />
+					</Link>
+					<Link
+						to="https://github.com/Judge-Paul"
+						className="text-blue-700 hover:text-blue-500 px-1.5 py-1 rounded-md"
+					>
+						<span className="sr-only">GitHub</span>
+						<FaGithub className="h-6 w-6" />
+					</Link>
+					<Link
+						to="https://hashnode.com/@jadge"
+						className="text-blue-700 hover:text-blue-500 px-1.5 py-1 rounded-md"
+					>
+						<span className="sr-only">Hashnode</span>
+						<FaHashnode className="h-6 w-6" />
+					</Link>
+					<Link
+						to="https://youtube.com/@jadge_dev23"
+						className="text-blue-700 hover:text-blue-500 px-1.5 py-1 rounded-md"
+					>
+						<span className="sr-only">Youtube</span>
+						<FaYoutube className="h-6 w-6" />
+					</Link>
+				</div>
+				<div className="text-center mt-10">
+					<Link
+						to="https://github.com/Judge-Paul/coverwrite-ai/issues"
+						className="mt-5 text-base text-blue-700"
+					>
+						Have a feature you want to add or noticed a bug?{" "}
+						<span className="hover:text-blue-500">Create an Issue</span>
+					</Link>
+				</div>
+				<p className="mt-5 text-center text-base text-blue-700">
+					&copy; {year} CoverWriteAI. All rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,42 +4,30 @@ import Hero from "../assets/hero.webp";
 import Helmet from "react-helmet";
 
 export default function Home() {
-  return (
-    <main className="bg-[#9fcaff] font-workSans px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-20">
-      <Helmet>
-        <title>CoverWriteAI - Home</title>
-      </Helmet>
-      <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/2 md:mt-16 lg:pr-20">
-          <h2 className="font-bold text-3xl md:text-6xl text-[#004fb6]">
-            <span className="md:block">Create an </span>
-            <span className="md:block">Amazing </span>
-            <span className="md:block">Cover Letter </span>
-            <span>Effortlessly</span>
-          </h2>
-          <img
-            src={Hero}
-            alt="Hero for homepage"
-            className="mt-12 w-full md:w-1/2 block md:hidden"
-          />
-          <p className="text-[#3a4688] font-semibold mt-8 text-lg">
-            Paste the text from the job posting into our tool. Our AI analyzes
-            the requirements and crafts a cover letter that sells you as the
-            ideal candidate.
-          </p>
-          <Link
-            to="/create"
-            className="text-[#fcfcd4] bg-[#004fb6] hover:bg-[#003d99] active:bg-[#003c99] font-bold px-20 py-3 rounded-full mt-8 inline-block w-full md:w-max text-center"
-          >
-            Get Started
-          </Link>
-        </div>
-        <img
-          src={Hero}
-          alt="Hero for homepage"
-          className="w-full md:w-1/2 hidden md:block"
-        />
-      </div>
-    </main>
-  );
+	return (
+		<main className="bg-[#9fcaff] font-workSans px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-20">
+			<Helmet>
+				<title>CoverWriteAI - Home</title>
+			</Helmet>
+			<div className="flex justify-center items-center">
+				<div className="text-center w-[56rem]">
+					<h2 className="font-bold text-3xl md:text-7xl text-[#004fb6]">
+						Create an Amazing Cover Letter Effortlessly
+					</h2>
+					<p className="text-[#3a4688] font-semibold mt-12 text-sm sm:text-lg">
+						Simply paste the text from the job posting into our free tool, and
+						add some details about yourself and watch our AI analyze the
+						requirement and carefully select information about you and craft the
+						perfect cover letter that sells you as the ideal candidate.
+					</p>
+					<Link
+						to="/create"
+						className="text-[#ffffff] bg-[#004fb6] hover:bg-[#003d99] active:bg-[#003c99] font-bold sm:px-28 py-4 rounded-full mt-12 inline-block w-full md:w-max text-center text-lg sm:text-2xl"
+					>
+						Get Started
+					</Link>
+				</div>
+			</div>
+		</main>
+	);
 }
